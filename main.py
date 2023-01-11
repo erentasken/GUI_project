1bc70c18aa48691839c283a91aa0adb1cb9360be
Okan Bulgur <okanbulgur@gmail.com>
2023-01-11 21:12:58 +0300

    version2

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
-from tkinter import *
+import tkinter as tk
 
-window = Tk()
-window.geometry("500x500")
-window.configure(background="grey")
+window = tk.Tk()
+button = tk.Button
+frame = tk.Frame
+menu = tk.Menu
 
 def drag_start(event):
     widget = event.widget
     widget.startX = event.x
     widget.startY = event.y
+
 def drag_motion(event):
     widget = event.widget
     x = widget.winfo_x() - widget.startX + event.x
     y = widget.winfo_y() - widget.startY + event.y
     widget.place(x=x, y=y)
 
-left_frame = Frame(window, bg = "black", width = 150, height =500)
-left_frame.pack(side = LEFT)
-
-label = Label(window, bg="red",width=10,height = 5)
-label.place(x = 0, y = 0)
-label.pack(side = TOP)
-label2 = Label(window,bg="blue",width=10,height=5)
-label2.place(x = 15, y = 0)
-label2.pack(side= BOTTOM)
+def create_label_rect():
+    _label = tk.Label(frame_cordinate, bg="white", borderwidth=1, relief="solid")
+    _label.place(relx=0, rely=0, width=150, height=75)
+    _label.bind("<Button-1>",drag_start)
+    _label.bind("<B1-Motion>",drag_motion)
 
 
-label.bind("<Button-1>",drag_start)
-label.bind("<B1-Motion>",drag_motion)
-label2.bind("<Button-1>",drag_start)
-label2.bind("<B1-Motion>",drag_motion)
+
+window.geometry("700x600+1200+80")
+window.title("Graphical Modeling Editor")
+
+
+
+frame_left = frame(window, bg="black")
+frame_left.place(relx=0, rely=0, relwidth=0.1, relheight=1)
+	
+
+frame_cordinate = frame(window, bg="white")
+frame_cordinate.place(relx=0.1, rely=0, relwidth=1, relheight=1)
+
+
+button_rectangular = button(frame_left, text="rect", activebackground="grey", command=create_label_rect)
+button_rectangular.place(relx=0.1, rely=0.1, relwidth=0.75, relheight=0.1)
+
+
+### MENU ###
+menubar = menu(window)
+filemenu = menu(menubar, tearoff=0)
+filemenu.add_command(label="New")
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+helpmenu = menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index")
+helpmenu.add_command(label="About...")
+menubar.add_cascade(label="Help", menu=helpmenu)
+window.config(menu=menubar)
+
 
 window.mainloop()
+
