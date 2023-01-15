2ecdef0c8947031b6596257e1524866496a3ccfb
erentasken <eren.tasken@gmail.com>
2023-01-15 16:49:25 +0300

    Images are fixed.

--- ButtonActivity.py
+++ ButtonActivity.py
@@ -1,8 +1,6 @@
 from tkinter import *
 import objectMovement
 
-
-'''
 def drag_start(event):
     widget = event.widget
     widget.startX = event.x
@@ -17,13 +15,10 @@ def drag_motion(event):
 def label_motion(_label):
     _label.bind("<Button-1>",drag_start)
     _label.bind("<B1-Motion>",drag_motion)
-'''
 
 def create_label(obj,screen):
     obj_img = PhotoImage(file="images\\"+obj+".png")
     _label = Label(screen, image=obj_img)
+    _label.img = obj_img 
     _label.place(relx=0, rely=0)
-    move = objectMovement.Movement(obj)
-
-
-
+    move = objectMovement.Movement(_label)
