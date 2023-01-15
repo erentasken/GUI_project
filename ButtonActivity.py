a2051a76e1bade1791ff568da87dee7012de5ba0
Okan Bulgur <okanbulgur@gmail.com>
2023-01-15 23:12:50 +0300

    Add text feature
    double click on objects opens text window and adds text property to objects

--- ButtonActivity.py
+++ ButtonActivity.py
@@ -1,11 +1,13 @@
 from tkinter import *
 import objectMovement
+import objectArrangement
+
 
 def create_label(obj, screen):
-    obj_text = "a"
-
-    obj_img = PhotoImage(file="images\\"+obj+".png")
+    obj_text = ""
+    obj_img = PhotoImage(file="images\\" + obj + ".png")
     _label = Label(screen, text=obj_text, image=obj_img, compound=CENTER)
-    _label.img = obj_img 
+    _label.img = obj_img
     _label.place(relx=0, rely=0)
     move = objectMovement.Movement(_label)
+    rearrange = objectArrangement.Arrangement(_label, screen)
