3774f9283dc074ce0ff39f2275f871dbdf1f0d05
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2023-01-15 17:38:26 +0300

    text added to image obj

--- ButtonActivity.py
+++ ButtonActivity.py
@@ -2,8 +2,10 @@ from tkinter import *
 import objectMovement
 
 def create_label(obj, screen):
+    obj_text = "a"
+
     obj_img = PhotoImage(file="images\\"+obj+".png")
-    _label = Label(screen, image=obj_img)
+    _label = Label(screen, text=obj_text, image=obj_img, compound=CENTER)
     _label.img = obj_img 
     _label.place(relx=0, rely=0)
     move = objectMovement.Movement(_label)
