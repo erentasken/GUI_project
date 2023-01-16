9552714460e4f0b0b1faf5aed38c15e3147bf9ac
Okan Bulgur <okanbulgur@gmail.com>
2023-01-17 01:10:46 +0300

    upgrade text class
    texti düzenlemek için text penceresini açınca objenin üstündeki text otomatik olarak pencereye yansıyor (düzenlemesi kolay olsun diye)

--- GUI.py
+++ GUI.py
@@ -1,10 +1,12 @@
-from tkinter import*
+from tkinter import *
 from tkinter import Canvas
 from functools import partial
 import objectBehaviours
+
+
 class GUI:
     def __init__(self):
-        self.objects = {"arrow", "rectangle"} # list of shapes
+        self.objects = {"arrow", "rectangle"}  # list of shapes
         self.window = Tk()
         self.window.geometry("700x600+1200+80")
         self.window.title("Graphical Modeling Editor")
@@ -12,30 +14,27 @@ class GUI:
         self.create_frame(self.window, self.coordinateSystem, self.objects)
         self.window.mainloop()
 
-    def CoordinateScreen(self): #method for right part of the existing screen.
+    def CoordinateScreen(self):  # method for right part of the existing screen.
         self.coordinateSystem = Canvas(self.window)
         self.coordinateSystem.place(relx=0.1, rely=0, relwidth=0.9, relheight=1)
 
     def create_label(self, obj, screen):
         self.obj = obj
         self.screen = screen
-        obj_text = ""
         obj_img = PhotoImage(file="images\\" + self.obj + ".png")
-        _label = Label(self.screen, text=obj_text, image=obj_img, compound=CENTER)
+        _label = Label(self.screen, image=obj_img, compound=CENTER)
         _label.img = obj_img
         _label.place(relx=0, rely=0)
         objectBehaviours.Movement(_label)
         objectBehaviours.TextInput(_label, self.screen)
 
-    def create_frame(self, screen, plotScreen, objects): #creates left-column puts the object's buttons as well.
+    def create_frame(self, screen, plotScreen, objects):  # creates left-column puts the object's buttons as well.
         j = 0
-        frame_left = Frame(screen, bg="black")
+        frame_left = Frame(screen, bg="#141E27")
         frame_left.place(relx=0, rely=0, relwidth=0.1, relheight=1)
 
         for i in objects:
             j += 0.2
-            button_obj = Button(frame_left, text=i, activebackground="grey",
+            button_obj = Button(frame_left, text=i, bg="#EEEDDE", activebackground="#203239",
                                 command=partial(self.create_label, i, plotScreen))
             button_obj.place(relx=0.1, rely=j, relwidth=0.75, relheight=0.1)
-
-
--- objectBehaviours.py
+++ objectBehaviours.py
@@ -1,26 +1,33 @@
 from tkinter import *
 from functools import partial
+
+
 class Movement:
     def __init__(self, obj):
         self.square_clicks = 0
         self.obj = obj
         self.motion_selector()
+
     def drag_start(self, e):
         widget = e.widget
         widget.startX = e.x
         widget.startY = e.y
+
     def drag_motion(self, e):
         widget = e.widget
         x = widget.winfo_x() - widget.startX + e.x
         y = widget.winfo_y() - widget.startY + e.y
         widget.place(x=x, y=y)
+
     def delete_object(self, e):
         widget = e.widget
         widget.destroy()
+
     def motion_selector(self):
         self.obj.bind("<Button-1>", self.drag_start)
         self.obj.bind("<B1-Motion>", self.drag_motion)
         self.obj.bind("<Button-3>", self.delete_object)
+
 
 class TextInput:
     def __init__(self, obj, screen):
@@ -34,13 +41,15 @@ class TextInput:
         self.obj.config(text=txt)
         self.frame.destroy()
 
-    def create_text(self, e):
-        textFrame = Canvas(self.screen, bg="black")
+    def _create_text(self, e):
+        textFrame = Canvas(self.screen)
         textFrame.place(relx=0.4, rely=0.8, relheight=0.2, relwidth=0.4)
         inputTxt = Text(textFrame)
+        inputTxt.insert(INSERT, self.obj.cget("text"))
         inputTxt.place(relheight=0.8, relwidth=1)
-        btn = Button(textFrame, text="✓", activebackground="#072B53", command=partial(self._printInput, inputTxt, textFrame))
+        btn = Button(textFrame, text="✓", bg="#EEEDDE", activebackground="#203239",
+                     command=partial(self._printInput, inputTxt, textFrame))
         btn.place(rely=0.8, relheight=0.2, relwidth=1)
 
     def inputTrigger(self):
-        self.obj.bind("<Double-Button-1>", self.create_text)
+        self.obj.bind("<Double-Button-1>", self._create_text)
